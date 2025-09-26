96d3dfe1267eddd573923165f6abe87f23d0bad7
Harsh Mittal <harshmittal.dev@gmail.com>
2025-09-26 22:17:51 +0530

    feat: implement layout and navigation components with theme support and analytics

---
+++ frontend/src/app/context/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ethersAdapter, projectId, networks } from "@/config";
+import { createAppKit } from "@reown/appkit/react";
+import React, { type ReactNode } from "react";
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+// Set up metadata
+const metadata = {
+  name: "Neurolend",
+  description:
+    "Neurolend a secure , smart and permission less  way to lend/borrow",
+  url: "https://www.neurolend.finance", // origin must match your domain & subdomain
+  icons: ["https://www.neurolend.finance/logo.png"],
+};
+
+// Create the modal
+export const modal = createAppKit({
+  adapters: [ethersAdapter],
+  projectId,
+  networks,
+  metadata,
+  chainImages: {
+    50312: "https://zerog.network/images/branding/zerog_logo_color.png",
+  },
+
+  themeMode: "dark",
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+    history: false,
+    swaps: false,
+    onramp: false,
+    send: false,
+    email: false,
+    socials: false,
+    receive: false,
+    pay: false,
+  },
+  themeVariables: {
+    "--w3m-accent": "#615C93",
+    "--w3m-border-radius-master": "8px",
+  },
+});
+
+function ContextProvider({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
+
+export default ContextProvider;
